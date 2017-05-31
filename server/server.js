5cc9ab5d31bd408339265714616ca3b85e71dd86
Sagar Wadekar <sagarwadekar2011@gmail.com>
2017-05-31 12:34:44 +0530

    API for post and get Todos

--- server/server.js
+++ server/server.js
@@ -10,21 +10,27 @@ app.use(bodyParser.json());
 
 app.post('/todo',( req, res) =>{
 
-console.log(req.body);
- var newTodo = new Todo({
-text:req.body.text,
-completed: req.body.completed
+        console.log(req.body);
+        var newTodo = new Todo({
+        text:req.body.text,
+        completed: req.body.completed
+        });
+
+        newTodo.save().then( (doc)=>{
+        res.send(doc);
+        }, (e)=>{
+            res.status(400).send(e);
+        });
+
 });
 
-newTodo.save().then( (doc)=>{
-   res.send(doc);
-}, (e)=>{
-    res.status(400).send(e);
+app.get('/todo',(req, res)=>{
+   Todo.find({}).then((todos)=>{
+       res.send(todos);
+   },(e)=>{
+       res.send(e)
+   });
 });
-
-
-
-})
 
 app.listen(3000, ()=>{
     console.log('Listening to the port 3000');
