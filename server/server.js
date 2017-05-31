876ba0517e4465df9e3c1de933dea7882d573393
Sagar Wadekar <sagarwadekar2011@gmail.com>
2017-05-31 15:51:19 +0530

    Find by ID and ID validation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -30,6 +31,19 @@ app.get('/todo',(req, res)=>{
    },(e)=>{
        res.send(e)
    });
+});
+
+app.get('/todo/:id',(req,res)=>{
+ var id = req.params.id;
+ if(!ObjectID.isValid(id)){
+   res.status(400).send('Invalid ID');
+ }
+ console.log(id);
+ Todo.findById(id).then((doc)=>{
+     res.send(doc);
+ }, (e)=>{res.send(e)
+ 
+ });
 });
 
 app.listen(3000, ()=>{
